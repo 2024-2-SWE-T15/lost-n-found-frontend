39454d326121e7981059c4e2c278165140e3b5c1
DropFL <dnjsah1230@gmail.com>
2024-11-26 16:23:33 +0900

    기본 line height 수정

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ const GlobalStyle = createGlobalStyle`
 
   #root {
     font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+    line-height: 1.5;
     background-color: white;
     color: #1e1e1e;
     width: 100vw;
